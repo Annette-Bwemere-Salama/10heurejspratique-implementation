ab9353ea563d8a76c02e112d213fbf6311e84ec8
annette BWEMERE <bwemereannette@gmail.com>
2022-07-01 22:34:47 +0200

    feat: ajout du javascript & html

--- quizApp/script.js
+++ quizApp/script.js
@@ -1,1 +1,40 @@
-const 
+const quizData = [
+    {
+        question: 'How old is Annette?',
+        a: '10',
+        a: '17',
+        a: '26',
+        a: '110',
+        correct: 'c'
+    }, {
+        question: 'What is the most user programming languaga in 2012?',
+        a: 'java',
+        b: 'C',
+        c: 'python',
+        d: 'Javascript',
+        correct: 'a'
+    }, {
+        question: 'Who is he president of Us?',
+        a: 'Florin Pop',
+        b: 'Donald Trump',
+        c: 'Ivan Saldano',
+        d: 'Mihai Andrei',
+        correct: 'b'
+    }, {
+        question: 'what does HTML stand for??',
+        a: 'Hypert Markup langanguage',
+        b: 'cascading StyLE Sheet',
+        c: 'Jason Objeect Notation',
+        d: 'Helicopters Terminals Motarboats Lamborginis',
+        correct: 'a'
+    }, {
+        question: 'What year was Javascript Launched?',
+        a: '1996',
+        b: '1995',
+        c: '1994',
+        d: 'none of the above',
+        correct: 'd'
+    }, {
+        
+    }
+]
