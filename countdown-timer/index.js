773b5c45a9868b050a6ca001eb6b82cc739b7c6c
annette BWEMERE <bwemereannette@gmail.com>
2022-07-01 13:35:03 +0200

    feat ajout jours,heure,minutes js

--- countdown-timer/index.js
+++ countdown-timer/index.js
@@ -4,10 +4,13 @@ function countdown() {
     const newYearsDate = new Date(newYears);
     const currentDate = new Date();
 
-    const seconds = (newYearsDate - currentDate) * 1000;
-    const days = Math.floor(seconds / 3600 / 24);
+    const seconds = new Date(newYearsDate - currentDate) / 1000;
 
-    console.log(newYearsDate - currentDate);
+    const days = Math.floor(seconds / 3600 / 24);
+    const hours = Math.floor(seconds / 3600) % 24;
+    const minutes = Math.floor(seconds / 60) % 24 % 60;
+
+    console.log(days, hours, minutes);
 }
 
 // initial call
