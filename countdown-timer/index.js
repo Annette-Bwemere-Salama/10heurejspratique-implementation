0bb90a2bea360715ffbe7a83ffc3dbbcea416007
annette BWEMERE <bwemereannette@gmail.com>
2022-07-01 12:04:42 +0200

    feat:ajout de la fonction countdown Pour le newYear

--- countdown-timer/index.js
+++ countdown-timer/index.js
@@ -1,5 +1,16 @@
-const newYears = '1st Jan 2011';
+const newYears = "1 Jan 2011";
 
 function countdown() {
+    const newYearsDate = new Date(newYears);
     const currentDate = new Date();
+
+    const seconds = (newYearsDate - currentDate) * 1000;
+    const days = Math.floor(seconds / 3600 / 24);
+
+    console.log(newYearsDate - currentDate);
 }
+
+// initial call
+countdown();
+
+setInterval(countdown, 1000);
